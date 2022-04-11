e7dc82e4887e175d2f14c0be8120e44a9b317196
jonakls <asmot54@hotmail.com>
2022-04-11 16:40:17 -0500

    feat: added instance and methods that help send titles and actionbars

--- bukkit/src/main/java/me/jonakls/kaleabukkit/KaleaBukkit.java
+++ bukkit/src/main/java/me/jonakls/kaleabukkit/KaleaBukkit.java
@@ -1,4 +1,23 @@
 package me.jonakls.kaleabukkit;
 
+import me.jonakls.kaleaapi.TitleModel;
+import me.jonakls.kaleabukkit.reflect.PacketsReflect;
+import org.bukkit.entity.Player;
+
 public class KaleaBukkit {
+
+    private PacketsReflect packetsReflect;
+
+    public KaleaBukkit() {
+        this.packetsReflect = new PacketsReflect();
+    }
+
+    public void sendTitle(Player player, TitleModel titleModel) {
+        this.packetsReflect.sendTitle(player, titleModel);
+    }
+
+    public void sendActionBar(Player player, String actionBar) {
+        this.packetsReflect.sendActionBar(player, actionBar);
+    }
+
 }
